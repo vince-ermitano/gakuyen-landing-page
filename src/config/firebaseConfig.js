50fbde1680a37c4f91d1d6ee359db9a7a9c024a1
Prince Vince <vincexermitano@gmail.com>
2023-08-14 03:06:24 -0700

    Add console.log

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfigString = process.env.REACT_APP_FIREBASE_CONFIG;
+console.log(firebaseConfigString);
 const firebaseConfig = JSON.parse(firebaseConfigString);
 
 // Initialize Firebase
