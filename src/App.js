964954e51e604066f7a8d91510da04d4df9034a2
Prince Vince <vincexermitano@gmail.com>
2023-08-13 21:14:00 -0700

    Finish landing page w/ full responsiveness

--- src/App.js
+++ src/App.js
@@ -6,46 +6,76 @@ function App() {
             <div className="landing-page-content">
                 <section className="two-column">
                     <div className="left-column">
-                        <div className="img-container">
-                            <img src="/theodyssey_366x100.png" alt="gakuyen" />
-                        </div>
-                        <div className="img-container">
-                            <img
-                                src="/onlineeducationportal.png"
-                                alt="gakuyen"
-                            />
+                        <div className="main-logo">
+                            <div className="img-container logo">
+                                <img
+                                    src="/theodyssey_366x100.png"
+                                    alt="gakuyen"
+                                />
+                            </div>
+                            <div className="img-container sub-logo">
+                                <img
+                                    src="/onlineeducationportal.png"
+                                    alt="gakuyen"
+                                />
+                            </div>
                         </div>
                         <div className="content-headings">
-                            <span>Creative Masterclass</span>
-                            <span>.</span>
-                            <span>Editing Assets</span>
-                            <span>.</span>
-                            <span>Freelance Tools</span>
+                            <span className="yellow-background">
+                                Creative Masterclass
+                            </span>
+                            <span className="no-mobile">.</span>
+                            <span className="yellow-background">
+                                Editing Assets
+                            </span>
+                            <span className="no-mobile">.</span>
+                            <span className="yellow-background">
+                                Freelance Tools
+                            </span>
                         </div>
-                        <span>Launching imminently...</span>
-                        <span>SIGN UP to be the first to know :)</span>
-                        <form>
-                            <input type="text" placeholder="Enter Name Here*" />
-                            <input
-                                type="email"
-                                placeholder="Enter Email Here..."
-                            />
-                            <div className="accept-terms">
-                                <input type="checkbox" />
-                                <label>I accept the terms and conditions</label>
+                        <div className="launching-w-form">
+                            <div id="launching-text">
+                                <span>Launching imminently...</span>
+                                <span>SIGN UP to be the first to know :)</span>
                             </div>
-                            <button type="submit">SUBMIT</button>
-                        </form>
+                            <form>
+                                <div className="text-inputs">
+                                    <input
+                                        type="text"
+                                        placeholder="Enter Name Here*"
+                                    />
+                                    <input
+                                        type="email"
+                                        placeholder="Enter Email Here..."
+                                    />
+                                </div>
+                                <div className="bottom-form-section">
+                                    <div className="accept-terms">
+                                        <input type="checkbox" />
+                                        <label>
+                                            I accept the terms &<br />{" "}
+                                            conditions
+                                        </label>
+                                    </div>
+                                    <button type="submit">SUBMIT</button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                     <div className="right-column">
                         <div className="img-container">
                             <img src="/macfinder.png" alt="gakuyen" />
                         </div>
                     </div>
+                    <div className="img-container" id="teachings-overtime">
+                        <img src="/teachingsovertime.png" alt="gakuyen" />
+                    </div>
+
+                    <div className="teachings-overtime-text mobile">
+                        <pre>(Teachings           Over          Time)          (Instructional       Interlude)           (Creative           Refinement)</pre>
+                    </div>
+
                 </section>
-                <div className="img-container" id="teachings-overtime">
-                    <img src="/teachingsovertime.png" alt="gakuyen" />
-                </div>
             </div>
         </div>
     );
